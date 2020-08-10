1f35df46131d096a95dee21a24307b0f4be8deb3
Aditya100704 <aditya100704@gmail.com>
2020-08-10 16:39:25 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let button = document.querySelector(".searchButton");
+button.addEventListener("click", onClick);
+
+let input = document.getElementById("search");
+
+
+
+function onClick(event) {
+    event.preventDefault();
+    console.log('button has been toggled')
+    input.value = "";
+}
