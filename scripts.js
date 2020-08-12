9ef11a12996585478d87d95687ba0cb321f55ade
Aditya100704 <aditya100704@gmail.com>
2020-08-12 17:24:27 -0400

    fourth commit

--- scripts.js
+++ scripts.js
@@ -9,5 +9,37 @@ function onClick(event) {
     event.preventDefault();
     console.log('button has been toggled')
     input.value = "";
-    window.location.hash = "body"
+
+}
+
+
+
+
+let db = firebase.database().ref();
+
+
+let buttonSub = document.getElementById('buttonSubscribe')
+
+buttonSub.addEventListener('click', updatedb);
+
+function updatedb(data) {
+    data.preventDefault();
+    let inputSub = document.querySelector(".inputSubscribe")
+
+    const email = inputSub.value;
+
+
+    inputSub.value = "";
+
+
+    console.log(email + " : " + "great job!");
+
+    //Update database here
+    let value = {
+        NAME: email,
+
+    };
+
+    db.push(value);
+
 }
